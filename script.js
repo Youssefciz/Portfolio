d9f9b7e47cd0ff180df4cde382f44a85c9f8b365
Youssef Sadek <159984494+Youssefciz@users.noreply.github.com>
2024-08-09 03:35:58 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+document.querySelectorAll('.scroll-link').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const fadeIns = document.querySelectorAll('.fade-in');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+
+    fadeIns.forEach(fadeIn => {
+        observer.observe(fadeIn);
+    });
+});
